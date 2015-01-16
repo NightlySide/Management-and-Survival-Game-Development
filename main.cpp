c0ae709d8d644305e08e7625af64f46f7479a871
NightlySide <froalexandre@gmail.com>
2015-01-16 17:32:15 +0100

    Adding Djikstra Algorithm
    Base but not finished Djikstra Algorithm

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
 #include "Inventory.h"
 #include "Character.h"
+#include "Djikstra.h"
+
+#include <list>
 
 using namespace std;
 
 int main()
 {
-    Character david = Character("David", "Human");
-    Inventory inv = david.getInventory();
-    Item lancePierre = Item("Lance pierre", "Arme", 10);
-
-    inv.addItem(lancePierre);
-
-    cout << david.getStatus() << endl;
-    cout << "End of programm." << endl;
-    cout << "Hello" << endl;
     return 0;
 }
