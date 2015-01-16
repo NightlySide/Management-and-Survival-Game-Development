419b63631c24a9afc373ab2995a67a7e68ff5e05
NightlySide <froalexandre@gmail.com>
2015-01-16 20:10:34 +0100

    Correcting main.cpp
    Correct merging problems

--- main.cpp
+++ main.cpp
@@ -24,15 +24,11 @@ int main(int argc, char *argv[])
         cout << "Can swim : " << animals[i].canSwim() << endl;
         cout << "+--------------------+" << endl << endl;
     }
-<<<<<<< HEAD
 
     Character perso;
     Item epee = Item("Epee","arme",40);
     perso.getInventory()->addItem(epee);
     cout << "Name: " << perso.getInventory()->getItemById(0).getName() << endl;
     cout << "Damage: " << perso.getInventory()->getItemById(0).getDamage() << endl;
-=======
-    cout << "End of test" << endl;
->>>>>>> origin/master
 	return 0;
 }
