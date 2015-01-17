0c4cb67fd6d07a9041a03d761517852597596f73
natanael67 <nathan.roth67@yahoo.fr>
2015-01-17 12:31:02 +0100

    SDL implementation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#include <SDL2/SDL.h>
+
 #include "Animal.h"
 #include "Character.h"
 
@@ -6,29 +10,64 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Animal gull = Animal("Gull", true, false);
-    Animal dog = Animal("Dog", false, false);
-    Animal gold_fish = Animal("Gold Fish", false, true);
-
-    Animal animals[3] = {gull, dog, gold_fish};
-
-	Animal test;
-	test = gull;
-
-    for (int i = 0; i<sizeof(animals)/sizeof(Animal); i++)
+	bool fullscreen=false;
+	/* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_EVENTS|SDL_INIT_TIMER) != 0 )
     {
-        cout << "+--------------------+" << endl;
-        cout << "Test avec animal " << i << endl;
-        cout << "Name : " << animals[i].getRace() <<endl;
-        cout << "Can fly : " << animals[i].canFly() << endl;
-        cout << "Can swim : " << animals[i].canSwim() << endl;
-        cout << "+--------------------+" << endl << endl;
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
     }
 
-    Character perso;
-    Item epee = Item("Epee","arme",40);
-    perso.getInventory()->addItem(epee);
-    cout << "Name: " << perso.getInventory()->getItemById(0).getName() << endl;
-    cout << "Damage: " << perso.getInventory()->getItemById(0).getDamage() << endl;
+    {
+        /* Création de la fenêtre */
+        SDL_Window* mWindow = NULL;
+        mWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
+
+        if( mWindow )
+        {
+            SDL_Event event;
+			while (SDL_WaitEvent(&event)) // Récupération des actions de l'utilisateur
+			{
+				switch(event.type)
+				{
+					case SDL_QUIT: // Clic sur la croix
+						SDL_DestroyWindow(mWindow);
+						SDL_Quit();
+						return 0;
+						break;
+					case SDL_KEYUP: // Relâchement d'une touche
+						if ( event.key.keysym.sym == SDLK_f ) // Touche f
+						{
+							// Alterne du mode plein écran au mode fenêtré
+							if (!fullscreen)
+							{
+								fullscreen = 1;
+								SDL_SetWindowFullscreen(mWindow,SDL_WINDOW_FULLSCREEN);
+							}
+							else if (fullscreen)
+							{
+								fullscreen = 0;
+								SDL_SetWindowFullscreen(mWindow,0);
+							}
+						}
+						break;
+				}
+			}
+
+            SDL_DestroyWindow(mWindow);
+        }
+        else
+        {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+
+    SDL_Quit();
+
+
 	return 0;
 }
