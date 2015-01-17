a1f6b72db5a9e19753dafc73184b4752549b1ad7
NightlySide <froalexandre@gmail.com>
2015-01-17 09:40:42 +0100

    Change from djikstra to A*
    Adding working A* class and an example in the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
-#include "Animal.h"
-#include "Character.h"
+#include "A_star.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    Animal gull = Animal("Gull", true, false);
-    Animal dog = Animal("Dog", false, false);
-    Animal gold_fish = Animal("Gold Fish", false, true);
-
-    Animal animals[3] = {gull, dog, gold_fish};
-
-	Animal test;
-	test = gull;
-
-    for (int i = 0; i<sizeof(animals)/sizeof(Animal); i++)
+        /*0: Stone
+        1: Herbe
+        2: Fleur
+        3: Mur
+        4: Arbre
+        5: Eau
+        6: Toit
+        7: Rock
+        8: Sentier
+        9: Vide*/
+    bool isPassable[10] = {true, true, true, false, false, false, false, false, true, false};
+    //no = 6,9,10
+    int tileMap[Y][X] = {{0,0,0,0},
+                         {0,0,3,3},
+                         {0,3,3,0},
+                         {0,0,0,0},
+                         {3,3,0,0},
+                         {0,0,0,3},
+                         {0,3,3,0},
+                         {0,0,0,0}
+                        };
+    vector<int> path = pathfinding(3, 27, tileMap, isPassable);
+    for (int i = 0; (unsigned)i<path.size(); i++)
     {
-        cout << "+--------------------+" << endl;
-        cout << "Test avec animal " << i << endl;
-        cout << "Name : " << animals[i].getRace() <<endl;
-        cout << "Can fly : " << animals[i].canFly() << endl;
-        cout << "Can swim : " << animals[i].canSwim() << endl;
-        cout << "+--------------------+" << endl << endl;
+        cout << path.at(i) << " ";
     }
-
-    Character perso;
-    Item epee = Item("Epee","arme",40);
-    perso.getInventory()->addItem(epee);
-    cout << "Name: " << perso.getInventory()->getItemById(0).getName() << endl;
-    cout << "Damage: " << perso.getInventory()->getItemById(0).getDamage() << endl;
 	return 0;
 }
